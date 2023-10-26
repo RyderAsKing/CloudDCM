d1c30b7ad2758443c019b7d516dd4aa78a18a402
RyderAsking <clasherlodt@gmail.com>
2023-10-27 02:24:48 +0530

    added owner_id to users table

---
+++ database/migrations/2023_10_26_205106_add_owner_id_to_users.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // the user that owns this user
+            $table
+                ->foreignId('owner_id')
+                ->nullable()
+                ->constrained('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->dropForeign('users_owner_id_foreign');
+        });
+    }
+};
