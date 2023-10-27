438030b90ce7872c8985c88236d4e09a04886e30
RyderAsking <clasherlodt@gmail.com>
2023-10-27 23:14:36 +0530

    bug fixes

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -37,11 +37,13 @@
                     <x-slot name="trigger">
                         <button
                             class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                            <div>{{ Auth::user()->name }}
+                            <div>
+                                {{ Auth::user()->name }}
                                 @foreach(Auth::user()->roles as $role)
                                 <span
                                     class="bg-gray-100 text-gray-800 text-xs font-semibold px-2.5 py-0.5 rounded-full">{{$role->name}}</span>
                                 @endforeach
+
                             </div>
 
                             <div class="ml-1">
