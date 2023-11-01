0527220239162837b7122bf56666330bfa99bb6b
RyderAsking <clasherlodt@gmail.com>
2023-11-02 03:44:11 +0530

    bug fixes

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -8,7 +8,7 @@
 @hasanyrole('subuser')
 <h1
     class="inline-flex items-center justify-center px-4 py-2 text-md font-medium tracking-wide text-white transition-colors duration-200 rounded-md bg-neutral-900 focus:ring-2 focus:ring-offset-2 focus:ring-neutral-900 focus:shadow-outline focus:outline-none">
-    >@if(auth()->user()->owner->company_name != null){{auth()->user()->owner->company_name}}
+    @if(auth()->user()->owner->company_name != null){{auth()->user()->owner->company_name}}
     @else {{ env("APP_NAME") }} @endif</h1>
 @endhasanyrole
 
