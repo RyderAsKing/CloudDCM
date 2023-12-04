d315924c1a799e0e5bbc950b5892fce2f6152d8b
RyderAsking <clasherlodt@gmail.com>
2023-12-05 00:36:44 +0530

    bug fix

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -73,7 +73,6 @@
                     @endhasrole
 
 
-                    @endhasrole
                     <button type="submit"
                         class="mt-2 inline-flex items-center justify-center px-4 py-2 text-sm font-medium tracking-wide text-white transition-colors duration-200 rounded-md bg-neutral-950 hover:bg-neutral-900 focus:ring-2 focus:ring-offset-2 focus:ring-neutral-900 focus:shadow-outline focus:outline-none"
                         style="width: fit-content;">Create user +</button>
