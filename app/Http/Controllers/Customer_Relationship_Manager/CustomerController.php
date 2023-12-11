a8b284886cad609b7d9bb544e496f1ff69e2820f
RyderAsking <clasherlodt@gmail.com>
2023-12-11 18:24:30 +0530

    customer relationship manager (CRM) contoller and views

---
+++ app/Http/Controllers/Customer_Relationship_Manager/CustomerController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers\Customer_Relationship_Manager;
+
+use App\Models\Customer;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $this->authorize('view', Customer::class);
+
+        return view('customer_relationship_manager.customers.index', [
+            'customers' => Customer::all(),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        $this->authorize('create', Customer::class);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $this->authorize('create', Customer::class);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        $customer = Customer::findOrFail($id);
+
+        $this->authorize('show', $customer, Customer::class);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+        $customer = Customer::findOrFail($id);
+
+        $this->authorize('update', $customer, Customer::class);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $customer = Customer::findOrFail($id);
+
+        $this->authorize('update', $customer, Customer::class);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $customer = Customer::findOrFail($id);
+
+        $this->authorize('delete', $customer, Customer::class);
+    }
+}
