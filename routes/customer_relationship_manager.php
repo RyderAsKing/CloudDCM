df1f1a4402fcbacf5ceccecc3971c16e34f4f7e1
RyderAsking <clasherlodt@gmail.com>
2023-12-11 18:25:04 +0530

    added customer_relationship_manager routes

--- routes/customer_relationship_manager.php
+++ routes/customer_relationship_manager.php
@@ -1,8 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Customer_Relationship_Manager\CustomerController;
 
 Route::prefix('customer_relationship_manager')
     ->name('customer_relationship_manager.')
     ->middleware('auth')
-    ->group(function () {});
+    ->group(function () {
+        Route::resource('customers', CustomerController::class);
+    });
