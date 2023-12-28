5c2ae881c730c36bff568ed89dff057109b6d43a
RyderAsking <clasherlodt@gmail.com>
2023-12-28 22:56:18 +0530

    bug fixes

--- resources/views/components/user/widget.blade.php
+++ resources/views/components/user/widget.blade.php
@@ -3,11 +3,12 @@
     <h2 class="text-xl font-semibold mb-2">Welcome to admin dashboard</h2>
     <p class="text-gray-700">Here you can manage users and their subusers</p>
 
-    <x-primary-link class="mt-2" href="{{ route('users.index') }}">
+    <x-primary-button href="{{ route('users.index') }}"
+        class="mt-4 inline-flex items-center justify-center px-4 py-2 text-sm font-medium tracking-wide text-white transition-colors duration-200 rounded-md bg-neutral-950 hover:bg-neutral-900 focus:ring-2 focus:ring-offset-2 focus:ring-neutral-900 focus:shadow-outline focus:outline-none">
         Manage
         now
         &rarr;
-    </x-primary-link>
+    </x-primary-button>
 </div>
 @endhasrole
 
@@ -17,14 +18,16 @@
     <p class="text-gray-700">Here are the modules you have access to </p>
 
     @can('view', App\Models\Rack::class)
-    <x-primary-link class="mt-2" href="{{ route('colocation_manager.locations.index') }}">
-        Colocation Manager &rarr; </x-primary-link>
+    <x-primary-button href="{{ route('colocation_manager.locations.index') }}"
+        class="mt-4 inline-flex items-center justify-center px-4 py-2 text-sm font-medium tracking-wide text-white transition-colors duration-200 rounded-md bg-neutral-950 hover:bg-neutral-900 focus:ring-2 focus:ring-offset-2 focus:ring-neutral-900 focus:shadow-outline focus:outline-none">
+        Colocation Manager &rarr; </x-primary-button>
     @endcan
 
     @can('view', App\Models\Customer::class)
-    <x-primary-link class="mt-2" href="{{route('customer_relationship_manager.customers.index')}}">
+    <x-primary-button href="{{route('customer_relationship_manager.customers.index')}}"
+        class="mt-4 inline-flex items-center justify-center px-4 py-2 text-sm font-medium tracking-wide text-white transition-colors duration-200 rounded-md bg-neutral-950 hover:bg-neutral-900 focus:ring-2 focus:ring-offset-2 focus:ring-neutral-900 focus:shadow-outline focus:outline-none">
         Customer Relationship Manager &rarr;
-    </x-primary-link>
+    </x-primary-button>
     @endhasrole
 </div>
 @endhasanyrole
@@ -32,11 +35,11 @@
 @hasrole('admin')
 <div class="mt-6 grid grid-cols-2 gap-2">
     <div class=" bg-white border rounded-lg shadow-sm p-7 border-neutral-200/60">
-        <x-primary-link href="#_" class="block mb-3">
+        <a href="#_" class="block mb-3">
             <h5 class="text-xl font-bold leading-none tracking-tight text-neutral-900">Total Users
                 Created
             </h5>
-        </x-primary-link>
+        </a>
         <p class="mb-4 text-neutral-500">There are a total of <strong>{{$users}}</strong>
             users created.
         </p>
