4380007248b5de2dde7a9d7b8784f1bc6fecb468
RyderAsking <clasherlodt@gmail.com>
2023-12-28 20:38:41 +0530

    changed from top navigation to side navigation

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,13 +1,13 @@
 @hasanyrole('admin|user')
 <h1
-    class="inline-flex items-center justify-center px-4 py-2 text-md font-medium tracking-wide text-white transition-colors duration-200 rounded-md bg-neutral-900 focus:ring-2 focus:ring-offset-2 focus:ring-neutral-900 focus:shadow-outline focus:outline-none">
+    class="inline-flex items-center justify-center px-3 py-2 text-lg font-medium tracking-wide text-white transition-colors duration-200 rounded-md bg-neutral-800 focus:ring-2 focus:ring-offset-2 focus:ring-neutral-900 focus:shadow-outline focus:outline-none">
     @if(auth()->user()->company_name != null){{auth()->user()->company_name}}
     @else {{ env("APP_NAME") }} @endif</h1>
 @endhasanyrole
 
 @hasanyrole('subuser')
 <h1
-    class="inline-flex items-center justify-center px-4 py-2 text-md font-medium tracking-wide text-white transition-colors duration-200 rounded-md bg-neutral-900 focus:ring-2 focus:ring-offset-2 focus:ring-neutral-900 focus:shadow-outline focus:outline-none">
+    class="inline-flex items-center justify-center px-3 py-2 text-lg font-medium tracking-wide text-white transition-colors duration-200 rounded-md bg-neutral-800 focus:ring-2 focus:ring-offset-2 focus:ring-neutral-900 focus:shadow-outline focus:outline-none">
     @if(auth()->user()->owner->company_name != null){{auth()->user()->owner->company_name}}
     @else {{ env("APP_NAME") }} @endif</h1>
 @endhasanyrole
@@ -16,11 +16,11 @@
 @if(file_exists(public_path('image/' . env('APP_LOGO'))) && env('APP_LOGO') != null)
 
 <img src="{{ asset('image/' . env('APP_LOGO')) }}" alt="{{ env(" APP_NAME") }}"
-    class="inline-flex items-center justify-center px-4 py-2 text-md font-medium tracking-wide text-white transition-colors duration-200 rounded-md bg-neutral-900 focus:ring-2 focus:ring-offset-2 focus:ring-neutral-900 focus:shadow-outline focus:outline-none"
+    class="inline-flex items-center justify-center px-3 py-2 text-lg font-medium tracking-wide text-white transition-colors duration-200 rounded-md bg-neutral-800 focus:ring-2 focus:ring-offset-2 focus:ring-neutral-900 focus:shadow-outline focus:outline-none"
     width="256">
 @else
 <h1
-    class="inline-flex items-center justify-center px-4 py-2 text-md font-medium tracking-wide text-white transition-colors duration-200 rounded-md bg-neutral-900 focus:ring-2 focus:ring-offset-2 focus:ring-neutral-900 focus:shadow-outline focus:outline-none">
+    class="inline-flex items-center justify-center px-3 py-2 text-lg font-medium tracking-wide text-white transition-colors duration-200 rounded-md bg-neutral-800 focus:ring-2 focus:ring-offset-2 focus:ring-neutral-900 focus:shadow-outline focus:outline-none">
     {{ env("APP_NAME") }}</h1>
 @endif
 @endguest
