990281a4f78aebc4e6c82f2ae450c53679223aa3
RyderAsking <clasherlodt@gmail.com>
2023-12-28 19:38:58 +0530

    showing the modules assigned

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -61,12 +61,41 @@
                                     <tr class="text-neutral-800 bg-white">
                                         <td class="px-5 py-4 text-sm font-medium whitespace-nowrap">{{$user->id}}
                                         </td>
-                                        <td class="px-5 py-4 text-sm font-medium whitespace-nowrap">{{$user->name}}
-                                            @foreach($user->roles as $role)
-                                            <span
-                                                class="bg-gray-100 text-gray-800 text-xs font-semibold px-2.5 py-0.5 rounded-full">{{$role->name}}</span>
-                                            @endforeach
+
+                                        <td
+                                            class="px-5 py-4 text-sm font-medium whitespace-nowrap flex justify-between">
+                                            {{$user->name}}
+                                            <div x-data="{
+                                                    tooltipVisible: false,
+                                                    tooltipText: '@foreach($user->roles as $role) | {{$role->name}}  @endforeach',
+                                                    tooltipArrow: true,
+                                                    tooltipPosition: 'top',
+                                                }"
+                                                x-init="$refs.content.addEventListener('mouseenter', () => { tooltipVisible = true; }); $refs.content.addEventListener('mouseleave', () => { tooltipVisible = false; });"
+                                                class="relative">
+                                                <div x-ref="tooltip" x-show="tooltipVisible"
+                                                    :class="{ 'top-0 left-1/2 -translate-x-1/2 -mt-0.5 -translate-y-full' : tooltipPosition == 'top', 'top-1/2 -translate-y-1/2 -ml-0.5 left-0 -translate-x-full' : tooltipPosition == 'left', 'bottom-0 left-1/2 -translate-x-1/2 -mb-0.5 translate-y-full' : tooltipPosition == 'bottom', 'top-1/2 -translate-y-1/2 -mr-0.5 right-0 translate-x-full' : tooltipPosition == 'right' }"
+                                                    class="absolute w-auto text-sm" x-cloak>
+                                                    <div x-show="tooltipVisible" x-transition
+                                                        class="relative px-2 py-1 text-white bg-black rounded bg-opacity-90">
+                                                        <p x-text="tooltipText"
+                                                            class="flex-shrink-0 block text-xs whitespace-nowrap"></p>
+                                                        <div x-ref="tooltipArrow" x-show="tooltipArrow"
+                                                            :class="{ 'bottom-0 -translate-x-1/2 left-1/2 w-2.5 translate-y-full' : tooltipPosition == 'top', 'right-0 -translate-y-1/2 top-1/2 h-2.5 -mt-px translate-x-full' : tooltipPosition == 'left', 'top-0 -translate-x-1/2 left-1/2 w-2.5 -translate-y-full' : tooltipPosition == 'bottom', 'left-0 -translate-y-1/2 top-1/2 h-2.5 -mt-px -translate-x-full' : tooltipPosition == 'right' }"
+                                                            class="absolute inline-flex items-center justify-center overflow-hidden">
+                                                            <div :class="{ 'origin-top-left -rotate-45' : tooltipPosition == 'top', 'origin-top-left rotate-45' : tooltipPosition == 'left', 'origin-bottom-left rotate-45' : tooltipPosition == 'bottom', 'origin-top-right -rotate-45' : tooltipPosition == 'right' }"
+                                                                class="w-1.5 h-1.5 transform bg-black bg-opacity-90">
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div x-ref="content"
+                                                    class="px-3 py-1 text-xs rounded-full cursor-pointer text-neutral-500 bg-neutral-100">
+                                                    modules
+                                                </div>
+                                            </div>
                                         </td>
+
                                         <td class="px-5 py-4 text-sm whitespace-nowrap">{{$user->email}}</td>
                                         <td class="px-5 py-4 text-sm whitespace-nowrap">
                                             @if ($user->email_verified_at != null)
