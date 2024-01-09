2db8094fe4532591950e5dc8a5b5993ef8a5e35c
Rajat Asthana <clasherlodt@gmail.com>
2024-01-10 00:04:01 +0530

    parent child relationship with subnets

--- app/Models/Subnet.php
+++ app/Models/Subnet.php
@@ -11,10 +11,26 @@ class Subnet extends Model
 
     protected $table = 'subnets';
 
-    protected $fillable = ['name', 'subnet', 'vlan', 'leased_company'];
+    protected $fillable = [
+        'name',
+        'subnet',
+        'vlan',
+        'leased_company',
+        'parent_id',
+    ];
 
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function children()
+    {
+        return $this->hasMany(Subnet::class, 'parent_id');
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(Subnet::class, 'parent_id');
     }
 }
