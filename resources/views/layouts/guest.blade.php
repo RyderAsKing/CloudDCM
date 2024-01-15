a41fdab47a976e47962d2a0c9c3917e672ec7968
Rajat Asthana <clasherlodt@gmail.com>
2024-01-15 23:30:24 +0530

    added background image to login (not using yet)

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -17,7 +17,10 @@
     </head>
 
     <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100"
+            @if(file_exists(public_path('image/' . env('BACKGROUND_LOGIN'))) && env('BACKGROUND_LOGIN') !=null)
+            style="background-image: url({{ asset('image/' . env('BACKGROUND_LOGIN')) }}); background-size: cover; background-position: center;"
+            @endif>
             <div>
                 <a href="/">
                     <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
