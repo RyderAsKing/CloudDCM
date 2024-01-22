ddd1dea68d646eb721ee605a77e19c0e853531f4
Rajat Asthana <clasherlodt@gmail.com>
2024-01-23 01:09:43 +0530

    bug fixes

--- app/Policies/LocationPolicy.php
+++ app/Policies/LocationPolicy.php
@@ -35,7 +35,8 @@ class LocationPolicy
 
         if (
             $user->hasRole('colocation_manager') ||
-            $user->hasRole('vps_manager')
+            $user->hasRole('vps_manager') ||
+            $user->hasRole('dedicated_server_manager')
         ) {
             return null;
         }
