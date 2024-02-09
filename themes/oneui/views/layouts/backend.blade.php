814ef51c430c0ecb8d5cd901fed07f3691d8234b
Rajat Asthana <clasherlodt@gmail.com>
2024-02-09 17:18:58 +0530

    added titles

--- themes/oneui/views/layouts/backend.blade.php
+++ themes/oneui/views/layouts/backend.blade.php
@@ -5,16 +5,13 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 
-        <title>OneUI - Bootstrap 5 Admin Template &amp; UI Framework</title>
+        <title>{{ config('app.name', 'CLOUDDCM') }}</title>
 
         <meta name="description" content="OneUI - Bootstrap 5 Admin Template &amp; UI Framework created by pixelcave">
         <meta name="author" content="pixelcave">
         <meta name="robots" content="index, follow">
 
         <!-- Icons -->
-        <link rel="shortcut icon" href="{{ asset('media/favicons/favicon.png') }}">
-        <link rel="icon" sizes="192x192" type="image/png" href="{{ asset('media/favicons/favicon-192x192.png') }}">
-        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('media/favicons/apple-touch-icon-180x180.png') }}">
 
         <!-- Modules -->
         @yield('css')
