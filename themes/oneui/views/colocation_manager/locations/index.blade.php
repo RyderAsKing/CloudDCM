c7885b4412e1f5e872af33482d56a4a672329ad3
Rajat Asthana <clasherlodt@gmail.com>
2024-02-16 23:26:46 +0530

    colocation manager dashboard (locations show)

---
+++ themes/oneui/views/colocation_manager/locations/index.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.backend')
+
+@section('content')
+<!-- Hero -->
+<div class="bg-body-light">
+    <div class="content content-full">
+        <div class="d-flex flex-column flex-sm-row justify-content-sm-between align-items-sm-center py-2">
+            <div class="flex-grow-1">
+                <h1 class="h3 fw-bold mb-1">
+                    Welcome to Colocation Manager
+                </h1>
+                <h2 class="fs-base lh-base fw-medium text-muted mb-0">
+                    Here you can manage locations and their racks
+                </h2>
+            </div>
+            <nav class="flex-shrink-0 mt-3 mt-sm-0 ms-sm-3" aria-label="breadcrumb">
+                <ol class="breadcrumb breadcrumb-alt">
+                    <li class="breadcrumb-item">
+                        <a class="link-fx" href="{{route('dashboard')}}">App</a>
+                    </li>
+                    <li class="breadcrumb-item" aria-current="page">
+                        Colocation Manager
+                    </li>
+                </ol>
+            </nav>
+        </div>
+    </div>
+</div>
+<!-- END Hero -->
+
+<!-- Page Content -->
+<div class="content content-boxed">
+    <div class="row items-push py-4">
+
+        @foreach($locations as $location)
+        <!-- Course -->
+        <div class="col-md-6 col-lg-4 col-xl-3">
+            <a class="block block-rounded block-link-pop h-100 mb-0" href="be_pages_elearning_course.html">
+
+                <div class="block-content block-content-full">
+                    <h4 class="h5 mb-1">
+                        @if(isset($location->name))
+                        {{$location->name}}
+                        @else
+                        Uncategorized
+                        @endif
+                    </h4>
+                    <div class="fs-sm text">{{isset($location->racks) ? $location->racks->count() : $location }}
+                        Racks added
+                    </div>
+                    <div class="fs-sm text-muted">{{isset($location->description) ? $location->description :
+                        '' }}
+
+                    </div>
+                </div>
+
+            </a>
+        </div>
+
+        @endforeach
+        <!-- END Course -->
+    </div>
+</div>
+<!-- END Page Content -->
+@endsection
